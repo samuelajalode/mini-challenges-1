8277d9dad6e918f90c9d24f6b97304511001a883
samuelajalode <113217825+samuelajalode@users.noreply.github.com>
2022-11-11 10:06:45 +0100

    Update index.js

--- src/transpose/index.js
+++ src/transpose/index.js
@@ -1,19 +1,20 @@
-/**
+ /**
  * Implement the solution in this function
  *
  * @param {number[]} array The array to transpose
  * @returns {number[]} The transposed array
  */
 function transpose(a) {
-
-
-  return a[0].map(function (_, c) { return a.map(function (r) { return r[c]; }); });
-  // or in more modern dialect
-  // return a[0].map((_, c) => a.map(r => r[c]));
+//my code here
+let newArray = [];
+for (let i = 0; i < a.length; i++) {
+    newArray.push([]);
+    for (let j = 0; j < a[i].length; j++) {
+        newArray[i].push(a[j][i]);
+    }
 }
-console.log(transpose([
-  [1, 3],
-  [2, 4],
-]));
-
+return newArray;
+}
+ 
+console.log(transpose([[1, 2, 3], [4, 5, 6], [7, 8, 9]]));
 module.exports = transpose;
